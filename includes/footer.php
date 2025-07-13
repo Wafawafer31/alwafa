8a4d16757ec54ca156c399ed602c7f47f4e2634a
Wafawafer31 <161691073+Wafawafer31@users.noreply.github.com>
2025-07-13 11:30:09 +0700

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<div class="site-footer">
+    <div class="container">
+         <div class="row justify-content-center text-center copyright">
+            <div class="col-md-8">
+                <p>Copyright &copy;<?php echo date('Y'); ?>. All Rights Reserved. &mdash; Designed by Alwafa Creative</p>
+            </div>
+        </div>
+    </div>
+</div>
