481f3f92359fffbffcb6eee39c167924051d5170
Wafawafer31 <161691073+Wafawafer31@users.noreply.github.com>
2025-07-13 11:30:41 +0700

    Create layout_end.php

---
+++ includes/layout_end.php
@@ -0,0 +1,6 @@
+    <script src="/alwafahub/assets/js/jquery-3.5.1.min.js"></script>
+    <script src="/alwafahub/assets/js/bootstrap.min.js"></script>
+    <script src="/alwafahub/assets/js/aos.js"></script>
+    <script src="/alwafahub/assets/js/custom.js"></script>
+</body>
+</html>
