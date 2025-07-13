e4aa9cd427b4645c20f684b413956a150243eb90
Wafawafer31 <161691073+Wafawafer31@users.noreply.github.com>
2025-07-13 10:59:34 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require_once 'includes/layout_start.php'; ?>
+<?php require_once 'includes/header.php'; ?>
+
+<div class="hero-slant overlay" style="background-image: url('assets/images/hero-min5.jpg');">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-lg-7 intro text-center">
+                <h1 class="text-white font-weight-bold mb-4">Welcome to AlwafaHub</h1>
+                <p class="text-white mb-4">Your moments, captured and delivered instantly.</p>
+                <p><a href="/alwafahub/client/demo-event" class="btn btn-primary">View Demo Event</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once 'includes/footer.php'; ?>
+<?php require_once 'includes/layout_end.php'; ?>
